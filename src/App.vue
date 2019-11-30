13c31d0f59bc1953fdc1950f7bd012150b9b580d
mengruolin <mengruolin@extracme.com>
2019-11-30 23:47:48 +0800

    What: init

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,13 @@
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {}
+</script>
+
 <style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
